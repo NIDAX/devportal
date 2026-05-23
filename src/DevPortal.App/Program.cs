98c39220ae5be3133e0e7d8f942edd5fa359ebbe
Leo <kafoussias@Leos-MacBook-Pro.local>
2026-05-23 21:32:41 +0200

    fix: add UseRouting for prometheus metrics endpoint

--- src/DevPortal.App/Program.cs
+++ src/DevPortal.App/Program.cs
@@ -4,5 +4,8 @@ using Prometheus;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDevPortalHealthCheck();
 var app = builder.Build();
+app.UseRouting();
+app.UseHttpMetrics();
+app.UseMetricServer();
 app.UseDevPortalHealthCheck();
 app.Run();
